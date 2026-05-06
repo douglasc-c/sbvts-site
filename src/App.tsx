f2f6cdbf2ef384ebeb6486ca00c1bf181f3e675b
Douglas Camargo <douglas.camargo.ltda@gmail.com>
2026-05-06 01:32:58 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { SiteLayout } from './components/SiteLayout'
 import { AboutPage } from './pages/AboutPage'
 import { GalleryPage } from './pages/GalleryPage'
@@ -6,14 +8,18 @@ import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
-    <Routes>
-      <Route element={<SiteLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<SiteLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+      <Analytics />
+      <SpeedInsights />
+    </>
   )
 }
 
